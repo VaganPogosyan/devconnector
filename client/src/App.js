b155ba9fe37988b44bffd95582dda8e809b66a52
Vagan Pogosyan <vagan2502@gmail.com>
2022-07-20 00:39:16 -0400

    renamed navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React, { Fragment, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/layout/Navbar';
+import NavBar from './components/layout/NavBar';
 import Landing from './components/layout/Landing';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
@@ -36,7 +36,7 @@ const App = () => {
     <Provider store={store}>
       <Router>
         <Fragment>
-          {/* <Navbar /> */}
+          <NavBar />
           <Alert />
           <Routes>
             <Route exact path='/' element={<Landing />} />
--- client/src/components/layout/NavBar.js
+++ client/src/components/layout/NavBar.js
@@ -4,7 +4,7 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { logout } from '../../actions/auth';
 
-const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
+const NavBar = ({ auth: { isAuthenticated, loading }, logout }) => {
   const authLinks = (
     <ul>
       <li>
@@ -56,7 +56,7 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
   );
 };
 
-Navbar.propTypes = {
+NavBar.propTypes = {
   logout: PropTypes.func.isRequired,
   auth: PropTypes.object.isRequired,
 };
@@ -65,4 +65,4 @@ const mapStateToProps = (state) => ({
   auth: state.auth,
 });
 
-export default connect(mapStateToProps, { logout })(Navbar);
+export default connect(mapStateToProps, { logout })(NavBar);
