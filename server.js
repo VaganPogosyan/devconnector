ef3b9d79ae726c3c8c9230653e900b0db1cce9ac
Vagan Pogosyan <vagan2502@gmail.com>
2022-07-20 00:17:08 -0400

    pre deployment

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ __dirname = path.resolve();
 // Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
-  app.use(express.static('client/build'));
+  app.use(express.static(path.join(__dirname, '/client/build')));
 
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
